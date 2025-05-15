5273d792a4f0548a9d2b7f009a24f05aa9591b75
`Muhammad Anas <i229808@nu.edu.pk>
2025-05-15 23:15:46 +0500

    app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import show_welcome
 # Set Streamlit page configuration
 st.set_page_config(
     page_title="FinVerse - Financial Realms Reimagined",
-    page_icon="assets/images/finverse_logo.png", width=90",
+    page_icon=" 🎨 ",
     layout="wide"
 )
 
